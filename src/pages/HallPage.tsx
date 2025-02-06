77f748b0b8816c039834b4dc3bbb2d09067250be
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-02-06 12:35:44 +0100

    Preparo la ui de hall page para que me muestre como minimo un usuario hardcodeado.

--- src/pages/HallPage.tsx
+++ src/pages/HallPage.tsx
@@ -1,6 +1,41 @@
+import { Link } from 'react-router-dom'
+
 function HallPage() {
-    return ( <>
-    <h1>hall page</h1></> );
+
+    const handleButtonClick = async () => {
+        console.log('handle')
+      };
+
+    const users = [
+        {
+        id: 1,
+        email: ""
+}]
+  
+  return (
+    <>
+      <div className="d-flex justify-content-center align-items-center">
+        <div className="container">
+          <ul className="list-group">
+            {users.map((user) => (
+              <li key={user.id} className="list-group-item">
+                <Link
+                  to={`/chat-room/${user.id}`}
+                >
+                  {user.email}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <div className="d-flex justify-content-center mt-3">
+            <a href="/" onClick={handleButtonClick}>
+              Salir
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default HallPage;
