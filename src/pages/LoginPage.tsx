65f1207440f7078ed167c124b6ea778f0bacd4ee
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-02-06 12:07:41 +0100

    Creo metodo para hacer signIn a firebase authentication.

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,11 +1,19 @@
 import { Link } from "react-router-dom";
 import LoginComponent from "../components/LoginComponent";
 import { FormValues } from "../types/globalTypes";
+import { auth } from "./../hooks/firebaseConfig";
+import { signInWithEmailAndPassword } from "firebase/auth";
+
 
 
 function LoginPage() {
 
-    const formSubmitted = (response: FormValues) => {
+    const formSubmitted = async (response: FormValues) => {
+
+        const userCredential = await signInWithEmailAndPassword(auth, response.email, response.password);
+        console.log("User logged in:", userCredential.user.uid);
+        // Crear un usuario en firebase AUthentication
+
         console.log('capturo desde login page el response', response);
     }
   return (
