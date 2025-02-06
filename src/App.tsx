06904504a841f9c884532e011878abc1c54c202a
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-02-06 10:34:28 +0100

    Codigo de Navegación

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import './App.css'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
- 
-
   return (
     <>
-    <h1>Hola</h1>
+      <Router>
+        <nav>
+          <Link to="/">Login</Link> 
+          <Link to="/register">Registro</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
