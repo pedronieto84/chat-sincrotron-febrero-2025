cb0b178a18dc61853d2dd882d766e3f7fce87e5b
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-02-09 15:25:37 +0100

    Creo los reducers.

--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -9,3 +9,35 @@ const initialState: IStore = {
     messages: []
 }
 
+// Defino los reducers para cada Action
+const reducer = (state = initialState, action: { type: string, payload: User | User[] | Message[] }) => {
+    switch (action.type) {
+        case LOGIN:
+            return {
+                ...state,
+                user: action.payload as User
+            }
+        case LOGOUT:
+            return {
+                ...state,
+                user: null
+            }
+        case REGISTER:
+            return {
+                ...state,
+                user: action.payload as User
+            }
+        case LOAD_USERS_HALL:
+            return {
+                ...state,
+                users: action.payload as User[]
+            }
+        case LOAD_MESSAGES:
+            return {
+                ...state,
+                messages: action.payload as Message[]
+            }
+        default:
+            return state
+    }
+}
