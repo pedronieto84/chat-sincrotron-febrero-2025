adf1bd4a0ea207d49d40520e0a17e89786c9e371
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-02-09 22:54:29 +0100

    Arreglo bug de test de poner mala contraseña en login. Y eso me ayuda a encontrar un bug de código por no manejar los errores de firebase.

--- cypress/e2e/chat-sincrotron/login.cy.js
+++ cypress/e2e/chat-sincrotron/login.cy.js
@@ -3,7 +3,7 @@
 describe('Login Page', () => {
     beforeEach(() => {
       // Visita la página de login antes de cada prueba
-      cy.visit('/login');
+      cy.visit('/');
     });
   
     it('should allow an existing user to log in', () => {
@@ -27,18 +27,31 @@ describe('Login Page', () => {
       cy.get('#root > div > form > div:nth-child(3) > button').should('be.disabled');
     });
   
-    it('should disable the submit button for incorrect password', () => {
-      // Rellena el formulario con un usuario existente pero con contraseña incorrecta
-      cy.get('#formEmail').type('pedro@gmail.com');
-      cy.get('#formPassword').type('123457');
-  
-      // Verifica que el botón de "Iniciar Sesión" esté deshabilitado
-      cy.get('#root > div > form > div:nth-child(3) > button').should('be.disabled');
-    });
+
+    
+    it('should return a 400 status code for incorrect password', () => {
+        // Intercepta la solicitud de red
+        cy.intercept('POST', '/login', (req) => {
+          req.reply((res) => {
+            // Verifica que el estado de la respuesta sea 400
+            expect(res.statusCode).to.equal(400);
+          });
+        }).as('loginRequest');
+    
+        // Rellena el formulario con un usuario existente pero con contraseña incorrecta
+        cy.get('#formEmail').type('pedro@gmail.com');
+        cy.get('#formPassword').type('123457');
+    
+        // Envía el formulario
+        cy.get('#root > div > form > div:nth-child(3) > button').click();
+    
+        // Espera a que la solicitud de red sea interceptada
+        cy.wait('@loginRequest');
+      });
   
     it('should navigate to "/register" when clicking "Crear Cuenta"', () => {
       // Verifica que el enlace "Crear Cuenta" esté presente
-      cy.contains('a', 'Crear Cuenta').click();
+      cy.contains('a', 'Crear cuenta').click();
   
       // Verifica que la navegación a la página de registro haya ocurrido
       cy.url().should('include', '/register');
