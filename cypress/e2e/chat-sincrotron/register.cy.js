925158f077e4f2f81a9232adf2c57ad30dbd783e
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-02-09 22:27:59 +0100

    Primer ejemplo de tests, fallan los 2 últimos, el primero por un bug de código. EL segundo por un bug de cypress o bug de testeo.

--- cypress/e2e/chat-sincrotron/register.cy.js
+++ cypress/e2e/chat-sincrotron/register.cy.js
@@ -20,4 +20,39 @@ describe('Register Page', () => {
       // Verifica que la navegación a la página de hall haya ocurrido
       cy.url().should('include', '/hall');
     });
+
+    it('should disable the submit button for invalid email', () => {
+      // Rellena el formulario con un email inválido
+      cy.get('#formEmail').type('invalid-email');
+      cy.get('#formPassword').type('password123');
+  
+      // Verifica que el botón de "Iniciar Sesión" esté deshabilitado
+      cy.get('#root > div > form > div:nth-child(3) > button').should('be.disabled');
+    });
+
+    it('should disable the submit button for too short password', () => {
+      // Rellena el formulario con una contraseña muy corta
+      cy.get('#formEmail').type(`${Date.now()}@example.com`);
+      cy.get('#formPassword').type('short');
+  
+      // Verifica que el botón de "Iniciar Sesión" esté deshabilitado
+      cy.get('#root > div > form > div:nth-child(3) > button').should('be.disabled');
+    });
+  
+    it('should disable the submit button for too long password', () => {
+      // Rellena el formulario con una contraseña muy larga
+      cy.get('#formEmail').type(`${Date.now()}@example.com`);
+      cy.get('#formPassword').type('thispasswordiswaytoolongtobeaccepted');
+  
+      // Verifica que el botón de "Iniciar Sesión" esté deshabilitado
+      cy.get('#root > div > form > div:nth-child(3) > button').should('be.disabled');
+    });
+  
+    it('should navigate to "/" when clicking "Crear Cuenta"', () => {
+      // Verifica que el enlace "Crear Cuenta" esté presente
+      cy.get('a[href="/"]').click();
+  
+      // Verifica que la navegación a la página de inicio haya ocurrido
+      cy.url().should('eq', Cypress.config().baseUrl + '/');
+    });
   });
