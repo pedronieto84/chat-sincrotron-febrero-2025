79d95156b9d960622341581d1f7b4b3ce7d2ed15
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-02-09 18:16:44 +0100

    Añado lógica a ChatPage para cargar los mensajes y engancharme a ellos mediante un selector. También paso al componente HIjo Chat, el array de mensajes.

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -1,40 +1,49 @@
 import ChatComponent from "../components/ChatComponent";
-import{ useParams} from "react-router-dom"
-import { Message } from "../types/globalTypes";
-import {db} from '../hooks/firebaseConfig';
+import { useParams } from "react-router-dom"
+import { IStore, Message } from "../types/globalTypes";
+import { db } from '../hooks/firebaseConfig';
 import { collection, addDoc } from "firebase/firestore";
 import { handleMessageFunction } from "../types/globalTypes";
-
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchMessagesObservable } from '../store/actions';
 
 
 
 function ChatPage() {
 
-    const {id} = useParams();
+    const { id  } = useParams()
+    const dispatch = useDispatch()
+    const messages = useSelector((state: IStore) => state.messages)
 
-    
-
-    const handleSendMessage: handleMessageFunction = async (message:Message) => {
-        console.log('capturo el mensaje del hijo desde el padre', message );
+    const handleSendMessage: handleMessageFunction = async (message: Message) => {
+        console.log('capturo el mensaje del hijo desde el padre', message);
         // Insercion del mensaje en la base de datos
-
-
         await addDoc(collection(db, `chats/${id}/chatroom`), message);
-            //const docRef = doc(db, "stats", 'globalStats');
     }
 
-    // UseEffect tiene que cargar mensajes y inseratarlos
+    useEffect(() => {
+        const unsubscribe = dispatch(fetchMessagesObservable(id as string) as any);
+        // Cleanup: Desuscribirse al desmontar el componente
+        return () => {
+            if (unsubscribe) {
+                console.log('me he UNSUBSCRIBED', unsubscribe);
+                unsubscribe();
+            }
+        }
+    }, [dispatch, id])
 
-    
+
     return (
 
         <>
-        <h1>{id}</h1>
-        <ChatComponent
-        handleConexionMessage={handleSendMessage}
-        />
-      </>
-      )
+            <h1>{id}</h1>
+            <ChatComponent
+                handleConexionMessage={handleSendMessage}
+                messages = {messages}
+            />
+        </>
+    )
 }
 
 export default ChatPage;
